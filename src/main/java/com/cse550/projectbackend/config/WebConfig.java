90bd37af290cff0d2c2c3eba0f6ec740f38b70e8
Ben Ferguson <bfergusonx64@outlook.com>
2023-11-29 13:51:59 -0500

    adjust cors config

--- src/main/java/com/cse550/projectbackend/config/WebConfig.java
+++ src/main/java/com/cse550/projectbackend/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowCredentials(false)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
